668c9b467d7b2ad695c67dc8067b78bdef78ab8c
Guest User <Guest@Epicodus-10C.local>
2018-01-19 13:43:14 -0800

    create form for input, add js to run input through a loop, that returns all numbers counting up to input number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
+
+//Business logic
 $(document).ready(function() {
-  
+  //Replace all non whole numbers with empty character when typed.
+  $('#numberInput').keyup(function() {
+    this.value = this.value.replace(/[^0-9]/g,'');
+  })
+  $('#beep-form').submit(function(event) {
+    event.preventDefault();
+    var numberInput = parseInt($('#numberInput').val());
+    var numberNumber = numberInput + 1;
+    var numberOutput = ["Beep! "];
+    console.log(numberNumber);
+    for (i = 1; i < numberNumber; i++) {
+      var counter = i.toString();
+      if (counter.indexOf("0") > -1) {
+        numberOutput.push("Beep! ");
+      } else if (counter.indexOf("1") > -1) {
+        numberOutput.push("Boop! ");
+      } else if (i === 0) {
+        numberOutput.push("Beep! ");
+      } else if (i % 3 === 0) {
+        numberOutput.push("I'm sorry, Dave. I'm afraid I can't do that. ");
+      } else {
+        numberOutput.push(counter + " ");
+      }
+    }
+    console.log(numberOutput);
+
+  });
+
+
+//User logic
+
+
 });
