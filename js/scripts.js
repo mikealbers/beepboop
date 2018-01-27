25a5047e5584ef1c6bf888ff928ca8fd29c1b62d
Guest User <Guest@Epicodus-10E.local>
2018-01-26 17:19:07 -0800

    seperate user and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
 //Business logic
+function beepBoop() {
+  var numberInput = parseInt($('#numberInput').val());
+  var numberNumber = numberInput + 1;
+  var numberOutput = [];
+  //HTML inserts
+  var listItems = ["<li>Beep!</li>","<li>Boop!</li>","<li>Quit trying to break it. Please enter a whole number.</li>","<li>I'm sorry, Dave. I'm afraid I can't do that.</li>","<li>Woah! That's a lot to compute! Try something smaller.</li>"]
+  //Loop through numbers counting up to input and push to array.
+  if (isNaN(numberNumber) === true) {
+    numberOutput = [listItems[2]];
+  } else if (numberNumber > 10000){
+    numberOutput = [listItems[4]]
+  } else {
+    for (i = 0; i < numberNumber; i++) {
+      var counter = i.toString();
+      if (counter.indexOf("0") > -1) {
+        numberOutput.push(listItems[0]);
+      } else if (counter.indexOf("1") > -1) {
+        numberOutput.push(listItems[1]);
+      } else if (i === 0) {
+        numberOutput.push("Beep! ");
+      } else if (i % 3 === 0) {
+        numberOutput.push(listItems[3]);
+      } else {
+        numberOutput.push("<li>" + counter + "</li>");
+      }
+    }
+  }
+  var resultList = numberOutput.join('');
+  return resultList;
+}
+
+//User
 $(document).ready(function() {
   //Replace all non whole numbers with empty character when typed.
   $('#numberInput').keyup(function() {
@@ -6,35 +38,7 @@ $(document).ready(function() {
   })
   $('#beep-form').submit(function(event) {
     event.preventDefault();
-    var numberInput = parseInt($('#numberInput').val());
-    var numberNumber = numberInput + 1;
-    var numberOutput = [];
-    //HTML inserts
-    var listItems = ["<li>Beep!</li>","<li>Boop!</li>","<li>Quit trying to break it. Please enter a whole number.</li>","<li>I'm sorry, Dave. I'm afraid I can't do that.</li>","<li>Woah! That's a lot to compute! Try something smaller.</li>"]
-
-    //Loop through numbers counting up to input and push to array.
-    if (isNaN(numberNumber) === true) {
-      numberOutput = [listItems[2]];
-    } else if (numberNumber > 10000){
-      numberOutput = [listItems[4]]
-    } else {
-      for (i = 0; i < numberNumber; i++) {
-        var counter = i.toString();
-        if (counter.indexOf("0") > -1) {
-          numberOutput.push(listItems[0]);
-        } else if (counter.indexOf("1") > -1) {
-          numberOutput.push(listItems[1]);
-        } else if (i === 0) {
-          numberOutput.push("Beep! ");
-        } else if (i % 3 === 0) {
-          numberOutput.push(listItems[3]);
-        } else {
-          numberOutput.push("<li>" + counter + "</li>");
-        }
-      }
-    }
-  //User logic
-    var resultList = numberOutput.join('');
-    $('#resultList').html(resultList);
+    beepBoop();
+    $('#resultList').html(beepBoop());
   });
 });
